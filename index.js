88651a6c6bda8268e95716435da46855663597d6
avnishranwa9122 <avnish.ranwa9122@softsensor.ai>
2024-01-21 00:18:51 +0530

    project setup

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("Welcome!!!");
+});
+
+app.listen(3000, (err) => {
+  if (!err) {
+    console.log("Server running...");
+  } else {
+    console.log("An error occured", err);
+  }
+});
